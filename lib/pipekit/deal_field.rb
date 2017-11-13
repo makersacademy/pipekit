c827849cf78f7b3ca1973d03f3e49c2e094ad1be
Roi Driscoll <roi.driscoll@gmail.com>
2017-11-13 11:43:06 +0000

    Fixes git merge duplication error

--- lib/pipekit/deal_field.rb
+++ lib/pipekit/deal_field.rb
@@ -1,7 +1,6 @@
 module Pipekit
   class DealField
     include FieldRepository
-    SINGULAR_CLASSNAME = "dealField".freeze
     # Pipedrive requires camelcase for resources
     SINGULAR_CLASSNAME = "dealField".freeze
     PLURALIZED_CLASSNAME = "dealFields".freeze
--- lib/pipekit/person_field.rb
+++ lib/pipekit/person_field.rb
@@ -1,7 +1,6 @@
 module Pipekit
   class PersonField
     include FieldRepository
-    SINGULAR_CLASSNAME = "personField".freeze
     # Pipedrive requires camelcase for resources
     SINGULAR_CLASSNAME = "personField".freeze
     PLURALIZED_CLASSNAME = "personFields".freeze

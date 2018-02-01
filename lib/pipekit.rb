5f7592cbdc7a4194a467780c70069ff009855962
guan <kuniyasu.guan@gmail.com>
2018-02-01 15:10:13 +0900

    Adds Pipeline object

--- lib/pipekit.rb
+++ lib/pipekit.rb
@@ -14,6 +14,7 @@ require "pipekit/deal_field"
 require "pipekit/note"
 require "pipekit/user"
 require "pipekit/stage"
+require "pipekit/pipeline"
 require "pipekit/activity"
 require "pipekit/resource_label"
 
